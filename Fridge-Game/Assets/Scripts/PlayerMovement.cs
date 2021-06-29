69731fea29487b6937820588b31ca8ba3ee11108
KraNf-png <63075003+KraNf-png@users.noreply.github.com>
2021-06-29 20:50:36 +0300

    Add jump force

--- Fridge-Game/Assets/Scripts/PlayerMovement.cs
+++ Fridge-Game/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float playerHeight = 2f;
+
     [Header("Player Movement Settings")]
     [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float airMultiplier = 0.4f;
+
+    [Header("Player Bind Keys")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Player Jump Force")]
+    [SerializeField] float jumpForce = 5f;
 
     float movementMultiplier = 10f;
-    float rbDrag = 6f;
+    float groundDrag = 6f;
+    float airDrag = 2f;
+
     float horizontalMovement;
     float verticalMovement;
+
+    bool isGrounded;
 
     Vector3 moveDirection;
 
@@ -26,13 +39,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 +   0.1f);
+
         MyInput();
         ControlDrag();
+
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            Jump();
+        }
     }
 
     void ControlDrag()
     {
-        rb.drag = rbDrag;
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
     }
 
     void MyInput()
@@ -43,6 +70,11 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
     }
 
+    void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
     private void FixedUpdate()
     {
         MovePlayer();
@@ -50,6 +82,13 @@ public class PlayerMovement : MonoBehaviour
 
     void MovePlayer()
     {
-        rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        if (isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        else if (!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
+        }
     }
 }
