17de493e294a47192013f87beac2a4eb5bcdf08a
KraNf-png <63075003+KraNf-png@users.noreply.github.com>
2021-07-05 15:15:46 +0300

    Script

--- Fridge-Game/Assets/Scripts/FoodPrep.cs
+++ Fridge-Game/Assets/Scripts/FoodPrep.cs
@@ -9,16 +9,17 @@ public class FoodPrep : MonoBehaviour
     private int timer = 0;
 
     Vector3 burgerScale = new Vector3(0.16835f, 0.16835f, 0.16835f);
+    
     private void OnTriggerEnter(Collider other)
     {
-        
+        Vector3 burgerPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.5f, gameObject.transform.position.z);
         if (other.name == "bread" && timer < 1)
         {
             timer++;
             Destroy(other.gameObject);
             Destroy(this.gameObject);
             this.burger.transform.localScale = burgerScale;
-            Instantiate(burger, this.gameObject.transform.position, Quaternion.identity);
+            Instantiate(burger, burgerPosition, Quaternion.identity);       
         }
     }
 }
